7dc28dcac8c3066cef670531056f9f83400ad97a
Gene Kelley <phpcodehead@gmail.com>
2025-02-06 16:22:16 -0600

    Add auth credentials file.

---
+++ auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'user' => 'root',
+        'password' => 'sid287!foP2ln',
+        'database' => 'login',
+    ]
+];
