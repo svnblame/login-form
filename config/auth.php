0f72ca0790462c664ac7a05a97fb1c55af1bc8c9
Gene Kelley <phpcodehead@gmail.com>
2025-02-07 14:56:13 -0600

    Create database connection

--- config/auth.php
+++ config/auth.php
@@ -5,7 +5,7 @@ return [
         'host' => 'localhost',
         'port' => 3306,
         'user' => 'root',
-        'password' => 'sid287!foP2ln',
+        'password' => 'foP2ln!sid287',
         'database' => 'login',
     ]
 ];
--- src/connection.php
+++ src/connection.php
@@ -1,1 +1,17 @@
 <?php
+$auth = require __DIR__ . "/../config/auth.php";
+
+// Database credentials
+define('DB_HOST', $auth['db']['host']);
+define('DB_NAME', $auth['db']['database']);
+define('DB_USER', $auth['db']['user']);
+define('DB_PASS', $auth['db']['password']);
+define('DB_PORT', $auth['db']['port']);
+
+// Create database connection
+try {
+    $conn = new PDO("mysql:host=" .DB_HOST . ";dbname=" .DB_NAME, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+    echo "DB Connection Error: " . $e->getMessage();
+    exit();
+}
